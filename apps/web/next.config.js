38125c1924110d3d6a67b06d2b50f11290dc1fb2
fabriciojunio <fabricioad444@gmail.com>
2026-05-27 18:38:24 -0300

    Desabilitar ESLint no build do Next.js para deploy na Vercel

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,6 +8,8 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: false },
   images: {
     formats: ['image/avif', 'image/webp'],
   },
